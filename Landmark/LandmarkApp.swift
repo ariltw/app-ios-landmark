33214fd18b01eb7db80f0e8d77c02fcdf9a1a073
ariltw <ariltw@live.com>
2020-09-24 20:52:58 +0700

    Initial Commit

---
+++ Landmark/LandmarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarkApp.swift
+//  Landmark
+//
+//  Created by Ariel Theodore W on 24/09/20.
+//
+
+import SwiftUI
+
+@main
+struct LandmarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
